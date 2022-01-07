cdd84165961ae2bde6f739a8d978dbb1cab2d2ff
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-07 15:10:13 +0300

    Create a custom pipe and define the logic for counting the days past in the pipe's .ts file.

---
+++ src/app/pipes/date-count.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateCount'
+})
+export class DateCountPipe implements PipeTransform {
+
+  transform(value: any): number {
+   let today: Date = new Date(); // get current date and time
+   let todayWithoutTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+
+   var dateDifference = Math.abs(todayWithoutTime - value); // returns value(value from the date declared in our array) in milliseconds
+   
+   const secondsInADay = 86400; //  60sec * 60min * 24hrs of the day
+
+   var dateDifferenceInSeconds = dateDifference / 1000; //  to convert the milliseconds to seconds
+   var daysCounter = dateDifferenceInSeconds / secondsInADay;
+
+   if (daysCounter >= 1 && value < todayWithoutTime) {
+     return daysCounter;
+   }
+   else {
+     return 0;
+   }
+  }
+
+}
