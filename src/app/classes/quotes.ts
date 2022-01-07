8ae97705ef764f1b6821129cc168c9e431ae02c4
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-07 06:51:52 +0300

    Create a constructor function to help define the initialization logic for creating an object and pass the quote properties as arguments of the constructor

--- src/app/classes/quotes.ts
+++ src/app/classes/quotes.ts
@@ -1,3 +1,11 @@
 export class Quotes {
-  
+  constructor (
+    public id: number,
+    public quote: string,
+    public postBy: string,
+    public author: string,
+    public postDate: Date,
+    public likes: number = 0,
+    public dislikes: number = 0
+  ) {}
 }
