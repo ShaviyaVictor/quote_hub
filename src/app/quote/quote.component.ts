ffa43d0f2c694a6e9c9ce93915185ea614599928
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-07 11:37:43 +0300

    Add font-awesome icons to our app and make it available to our app via imports to the root styles file

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -6,16 +6,13 @@ import { Quotes } from '../classes/quotes';
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
+
 export class QuoteComponent implements OnInit {
-
+  
   quotes: Quotes[] = [
-
     new Quotes (1, 'Either write something worth reading or do something worth writing.', 'Victor Shaviya', 'Benjamin Franklin', new Date(2022,1,7)),
-
     new Quotes (2, 'Graduation is an exciting time. It marks both an ending and a beginning; it\’s warm memories of the past and big dreams for the future.', 'Josphine Mbaisi', 'UWI, Mona', new Date(2021,1,10)),
-
     new Quotes (3, 'When the last point is done, we are humans. Give your opponent a hug and say, \'great fight,\' and that\'s all.', 'Norris Ambune', 'Novak Djokovic', new Date(2021,11,3))
-    
   ]
 
   constructor() { }
